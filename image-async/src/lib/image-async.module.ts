8fbda99fa08f388948f55989aee30a071b1f0739
senthurai <senthurai@gmail.com>
2020-02-07 14:56:29 -0500

    Create image-async.module.ts

---
+++ image-async/src/lib/image-async.module.ts
@@ -0,0 +1,10 @@
+import { NgModule } from '@angular/core';
+import { ImageAsyncComponent } from './image-async.component';
+
+@NgModule({
+  declarations: [
+    ImageAsyncComponent
+  ],
+  exports: [ImageAsyncComponent]
+})
+export class ImageAsyncModule { }
